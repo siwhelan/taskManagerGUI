e51ae74cf63d620b07e31c66a85055f9fe22324b
Simon Whelan <74879900+siwhelan@users.noreply.github.com>
2023-01-21 09:26:50 +0000

    Update loginPage.py

--- loginPage.py
+++ loginPage.py
@@ -124,3 +124,16 @@ button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 app.bind("<Return>", lambda event: button.invoke())
 
 app.mainloop()
+# Create admin account
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["task_manager"]
+collection = db["login_info"]
+
+# Hash the password using SHA-256
+hashed_password = hashlib.sha256("adm1n".encode("utf-8")).hexdigest()
+
+# Create the admin account document
+admin_account = {"username": "admin", "password": hashed_password}
+
+# Insert the admin account document into the collection
+collection.insert_one(admin_account)
